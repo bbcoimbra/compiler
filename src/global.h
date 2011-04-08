b00e03a597b9f56fecaadb9f5d5ebeb45d4f94bf
Bruno Coimbra <bbcoimbra@gmail.com>
2011-04-08 11:10:26 -0300

    fix structs

--- src/global.h
+++ src/global.h
@@ -1,20 +1,22 @@
 #ifndef _GLOBAL_H_
 #define _GLOBAL_H_
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #define MAX_CHILDREN 3
-#define YYDEBUG 1
 
 typedef enum {stmt_k, expr_k} node_kind;
 typedef enum {if_k, while_k, attrib_k, write_k, read_k} stmt_kind;
 typedef enum {op_k, id_k, const_k} exp_kind;
 typedef enum {Void, Integer, Boolean} exp_type;
 
-typedef struct tree_node_t {
+struct token_t {
+	int type;
+	union {
+		int num;
+		char *name;
+	} value;
+};
+
+struct tree_node_t {
 	struct tree_node_t *child[MAX_CHILDREN];
 	struct tree_node_t *next;
 	int lineno;
@@ -27,7 +29,7 @@ typedef struct tree_node_t {
 		char *name;
 	} attr;
 	exp_type expr_type;
-} tree_node;
+};
 
 #endif /* _GLOBAL_H_ */
 
