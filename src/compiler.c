96c70ba6c9fdf9fdd35a077ebe3e52591b92c3a2
Bruno Coimbra <bbcoimbra@gmail.com>
2011-04-13 02:20:59 -0300

    fix call to yyparse and improve error handling

--- src/compiler.c
+++ src/compiler.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "global.h"
+#include "util.h"
 #include "parser.h"
 
 FILE *yyin;
@@ -11,7 +12,11 @@ int main(int argc, char **argv, char **env)
 	struct tree_node_t *ast;
 
 	yyin = fopen(argv[1], "r");
-	ast = (struct tree_node_t *) yyparse();
+	if (yyparse(ast) != 0)
+	{
+		fprintf(stderr, "Compilation failure!\n");
+		exit(EXIT_FAILURE);
+	}
 
 	return EXIT_SUCCESS;
 
