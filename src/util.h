ee8957d9f815b0fc5df939d9f89ec8e5c64c8488
Bruno Coimbra <bbcoimbra@gmail.com>
2011-04-18 18:48:33 -0300

    included print_tree2 to test pre_order()

--- src/util.h
+++ src/util.h
@@ -5,6 +5,7 @@ struct node_t * new_expr_node (enum expr_kind kind);
 struct node_t * new_stmt_node (enum stmt_kind kind);
 void print_node (struct node_t * node);
 void print_tree (struct node_t * n);
+void print_tree2 (struct node_t * n);
 
 char *copy_str(char * str);
 
