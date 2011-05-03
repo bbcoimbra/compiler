4244c56453f2eff5a441732d714ce13365118d66
Bruno Coimbra <bbcoimbra@gmail.com>
2011-05-02 23:21:35 -0300

    include getopt() to use option flags

--- src/compiler.c
+++ src/compiler.c
@@ -12,7 +12,40 @@ void usage (char * arg);
 int main(int argc, char **argv, char **env)
 {
 
-	yyin = fopen(argv[1], "r");
+	FILE *cfile, *dotfile;
+	int print_st = 0, print_ast = 0;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "std:c:")) != -1)
+	{
+		switch(opt)
+		{
+			case 's':
+				print_st = 1;
+				break;
+			case 't':
+				print_ast = 1;
+				break;
+			case 'd':
+				dotfile = fopen(optarg, "w");
+				break;
+			case 'c':
+				cfile = fopen(optarg, "w");
+				break;
+			default:
+				usage(argv[0]);
+				exit(EXIT_FAILURE);
+		}
+	}
+
+	if (optind >= argc)
+	{
+		fprintf (stderr, "Error: missed  input file.\n");
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	yyin = fopen(argv[optind], "r");
 	if (yyparse() != 0)
 	{
 		fprintf(stderr, "Compilation failure!\n");
