597eff6c3a8bf49bc5abb4f1c2b2d73c76dad39a
Bruno Coimbra <bbcoimbra@gmail.com>
2011-05-25 20:08:47 -0300

    adjusts on functions declarations

--- src/dotgen.c
+++ src/dotgen.c
@@ -5,6 +5,12 @@
 #include "dotgen.h"
 
 char * gen_name(struct node_t *node);
+void dot_emit_preamble (FILE * file);
+void dot_gen_graph (FILE * file, struct node_t * node, struct node_t * context);
+void dot_gen_shapes (FILE * file,	struct node_t * node);
+void dot_emit_finally (FILE * file);
+void dot_emit_if (FILE * file, struct node_t * node,	struct node_t * context);
+void dot_emit_while (FILE * file, struct node_t * node,	struct node_t * context);
 
 void generate_dot (FILE * file, struct node_t * ast)
 {
--- src/dotgen.h
+++ src/dotgen.h
@@ -4,12 +4,6 @@
 #include <stdio.h>
 #include "global.h"
 
-void dot_emit_preamble (FILE * file);
-void dot_gen_graph (FILE * file, struct node_t * node, struct node_t * context);
-void dot_gen_shapes (FILE * file, struct node_t * node);
-void dot_emit_finally (FILE * file);
-
-void dot_emit_if (FILE * file, struct node_t * node, struct node_t * context);
-void dot_emit_while (FILE * file, struct node_t * node, struct node_t * context);
+void generate_dot (FILE * file, struct node_t * ast);
 
 #endif /* _DOTGEN_H_ */
